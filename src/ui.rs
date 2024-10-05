5a0d9db6f37bcf56a300ed4771143848f066901f
henry <devgony@gmail.com>
2024-10-05 14:46:20 +0900

    feat: draw ui with manual grid

--- src/ui.rs
+++ src/ui.rs
@@ -12,48 +12,17 @@ pub fn ui(frame: &mut Frame, app: &App) {
     let schemas = app.get_schemas();
     // a schema will take 20 of width and the number of column_defs of height
 
-    let main = Layout::default()
+    let [title_rect, main_rect] = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Length(3), Constraint::Min(0)])
-        .split(frame.area());
+        .areas(frame.area());
 
-    let panes = Layout::default()
+    let [sql_rect, erd_rect] = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(30), Constraint::Percentage(70)])
-        .split(main[1]);
+        .areas(main_rect);
 
-    let erd_pane = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Max(20), Constraint::Max(0)])
-        .split(panes[1]);
-
-    let title_block = Block::default()
-        .borders(Borders::ALL)
-        .style(Style::default());
-
-    let title =
-        Paragraph::new(Text::styled("nERD", Style::default().fg(Color::Green))).block(title_block);
-
-    let sql_pane = Paragraph::new(Text::styled(
-        app.sql_text.clone(),
-        Style::default().fg(Color::Green),
-    ))
-    .block(Block::bordered());
-
-    // should be node-graph widget
-    let erd_block = Block::default()
-        .borders(Borders::ALL)
-        .style(Style::default());
-
-    // let erd = Paragraph::new(Text::styled("Some ERD", Style::default().fg(Color::Green)))
-    //     .block(erd_block);
-
-    frame.render_widget(title, main[0]);
-    frame.render_widget(sql_pane, panes[0]);
-    // frame.render_widget(erd_block, columns[1]);
-    // frame.render_widget(erd, columns[1]);
-
-    let items = ["Item 1", "Item 2", "Item 3"];
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
 
     let list = List::new(items)
         .block(Block::bordered().title("Table1"))
@@ -63,5 +32,49 @@ pub fn ui(frame: &mut Frame, app: &App) {
         .repeat_highlight_symbol(true)
         .direction(ListDirection::TopToBottom);
 
-    frame.render_widget(list, erd_pane[0]);
+    let table_rects = Layout::horizontal([Constraint::Length(20); 4])
+        // .vertical_margin(1)
+        // .horizontal_margin(2)
+        .split(erd_rect)
+        .into_iter()
+        .for_each(|column| {
+            Layout::vertical([Constraint::Min(50); 6])
+                .margin(1)
+                .split(*column)
+                .into_iter()
+                .for_each(|y| {
+                    frame.render_widget(list.clone(), *y);
+                });
+        });
+    // .flatten()
+    // .collect::<Vec<&Rect>>();
+
+    let title_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+    let title =
+        Paragraph::new(Text::styled("nERD", Style::default().fg(Color::Green))).block(title_block);
+
+    let sql_paragrah = Paragraph::new(Text::styled(
+        app.sql_text.clone(),
+        Style::default().fg(Color::Green),
+    ))
+    .block(Block::bordered().title("SQL pane"));
+
+    // should be node-graph widget
+    let erd_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    // let erd = Paragraph::new(Text::styled("Some ERD", Style::default().fg(Color::Green)))
+    //     .block(erd_block);
+
+    frame.render_widget(title, title_rect);
+    frame.render_widget(sql_paragrah, sql_rect);
+    // use scrollable widget instead of block
+    frame.render_widget(Block::bordered().title("ERD pane"), erd_rect);
+    // frame.render_widget(erd_block, columns[1]);
+    // frame.render_widget(erd, columns[1]);
+
+    // frame.render_widget(list, table_rect[0]);
 }
